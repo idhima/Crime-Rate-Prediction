c69c13b8e2738f5b2b3d6733b04b088cc4c9df79
Ridhima <162116052+idhima@users.noreply.github.com>
2024-11-13 14:34:35 +0530

    Create data_preprocessing.h

---
+++ Data Preprocessing/data_preprocessing.h
@@ -0,0 +1,9 @@
+#ifndef DATA_PREPROCESSING_H
+#define DATA_PREPROCESSING_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::vector<float>> preprocessData(const std::string &filePath);
+
+#endif // DATA_PREPROCESSING_H
